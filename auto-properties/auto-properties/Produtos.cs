5ab64f9ba8a0a6bcb8580769b469c9e9ae31cb6c
Carlos Abraao <carlos.programadorr@gmail.com>
2024-01-22 17:28:17 -0300

    uso do autoProperties

---
+++ auto-properties/auto-properties/Produtos.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+namespace auto_properties
+{
+    internal class Produtos
+    {
+
+        private string _nome;
+
+        // posso pegar o valor do preço mais não posso edita-lo
+        public double Preco {  get; private set; }
+
+        // Construtor com argumentos
+        // estou fixando um valor no preço
+        public Produtos(string nome)
+        {
+            this._nome = nome;
+            this.Preco = 580;
+        }
+
+        // Construtor sem argumentos
+
+        public Produtos()
+        {
+
+        }
+
+        // Propeties
+        public string Nome { 
+        
+           get { return _nome; }       
+           set { _nome = value; }
+        }
+
+        // Get 
+
+        //public String GetNome(String nome)
+        //{
+        //    return _nome;
+        //}
+
+
+        //// Set 
+        public void SetNome(String nome)
+        {
+            _nome = nome;
+        }
+
+        public override string ToString()
+        {
+            return ("Nome:  " + _nome + "Preço: " + Preco);
+        }
+        
+
+        
+    }
+}
---
+++ auto-properties/auto-properties/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using auto_properties;
+
+namespace autoProperties
+{
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produtos p = new Produtos("Geladeira");
+            // Alterando nome e preço pelo AUTO PROPERTIES  e PROPERTIES
+
+            p.SetNome("Computador");
+            
+            
+            Console.WriteLine(p.Preco);
+        }
+    }
+}
